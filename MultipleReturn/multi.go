204f40ad8437763288db12a7600c560e98e7997d
Nikhil <nikhilyadav9305@gmail.com>
2025-07-03 19:14:53 +0530

    "Go Lang"

--- MultipleReturn/multi.go
+++ MultipleReturn/multi.go
@@ -1,14 +1,27 @@
 package main
 
-import("fmt")
+import("fmt"
+		"strconv"
+)
 
 func swap(a, b int) (int, int) {
 	return b, a
 }
 
+// returning result and error together 
+func convert(str string) (int, error) {
+	result, err := strconv.Atoi(str)
+	return result, err
+}
 func main() {
 	
 	a, b := swap(4, 5)
 	fmt.Println(a, b)
 	
+	num, err := convert("1234")
+	if(err == nil) {
+		fmt.Println("String converted to integer: ",num)
+	}else{
+		fmt.Println("Error:", err)
+	}
 }
